0bc0f2cac7094feccc635b15ea99932fffa1dd16
Ильяс Сингатуллов <daniel.rafoo@mail.ru>
2018-11-28 23:56:46 +0300

    Я добавил кнопук

--- kursach/Form1.cs
+++ kursach/Form1.cs
@@ -16,5 +16,11 @@ namespace kursach
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
     }
 }
